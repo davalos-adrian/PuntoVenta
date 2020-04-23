9e5f8e0adea6d0ea395699cb5c2ddc8f0c7b30b4
Adrian Davalos <aalejandro.davalos@alumnos.udg.mx>
2020-04-22 22:52:07 -0500

    Test2

--- login.php
+++ login.php
@@ -1,4 +1,17 @@
 <?php
 	include "db_con.php";
-	echo $_POST['email'];
+	session_start();
+	$email = $_POST['email'];
+	$password = hash('sha256', $_POST['password']);
+	$stmt = $pdo->prepare("
+			SELECT *
+			FROM employees
+			WHERE email = ? AND password = ?");
+	$stmt->execute(array($email, $password));
+	$res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	if (count($res) > 0) 
+	{
+		$_SESSION['user_id'] = $res[0]['id'];
+		$_SESSION['username'] = $email;
+	}
 ?>
