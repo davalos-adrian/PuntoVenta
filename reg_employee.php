8299271f64d31e48daf184e3d45c70bb14cb3cd6
Adrian Davalos <aalejandro.davalos@alumnos.udg.mx>
2020-04-23 01:06:24 -0500

    Login repaired

--- reg_employee.php
+++ reg_employee.php
@@ -1,11 +1,16 @@
 <?php
+	require_once __DIR__ . '/vendor/autoload.php';
+	$loader = new \Twig\Loader\FilesystemLoader('./views');
+	$twig = new \Twig\Environment($loader, [
+	    'cache' => false,
+	]);
 	session_start();
 	if(!isset($_SESSION['username']))
 	{
 		header('Location: index.php');
 		die();
 	} else {
-		echo $GLOBALS['twig']->render('body.html', 
+		echo $twig->render('body.html', 
 									[
 										'title' => 'Registro empleados',
 										'content' => 'clients.html',
