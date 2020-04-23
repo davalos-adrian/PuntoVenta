f831c798b01f5dd8cba88b908e1980f96298e901
Adrian Davalos <aalejandro.davalos@alumnos.udg.mx>
2020-04-23 12:30:17 -0500

    Panel test

--- reg_product.php
+++ reg_product.php
@@ -1,19 +1,19 @@
 <?php
 	require_once __DIR__ . '/vendor/autoload.php';
 	$loader = new \Twig\Loader\FilesystemLoader('./views');
-	$GLOBALS['twig'] = new \Twig\Environment($loader, [
+	$twig = new \Twig\Environment($loader, [
 	    'cache' => false,
 	]);
 	session_start();
 	if(!isset($_SESSION['username']))
 	{
-		$attempt = isset($_GET['login_attempt']) ? $_GET['login_attempt'] : 0;
-		echo $GLOBALS['twig']->render('login.html', ["attempt" => $attempt]);
+		header('Location: index.php');
+		die();
 	} else {
-		echo $GLOBALS['twig']->render('body.html', 
+		echo $twig->render('body.html', 
 									[
-										'title' => 'Inicio',
-										'content' => 'clients.html',
+										'title' => 'Registro productos',
+										'content' => 'reg_product.html',
 										'username' => $_SESSION['username']
 									]);
 	}
